749658e51cf93995df8b378fe53be7cd4195ed8c
Sho Nakatani <lay.sakura@gmail.com>
2021-05-29 20:26:18 +0900

    ci: deploy to GitHub pages (#2)
    * build: GitHub repo & pages settings
    
    * ci: adds deploy rule on merging main branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://eukarya-inc.github.io',
+  baseUrl: '/apllodb-doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'eukarya-inc', // Usually your GitHub org/user name.
+  projectName: 'apllodb-doc', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
