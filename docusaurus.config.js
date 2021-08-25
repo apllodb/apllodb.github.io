b2b23917be0017dd80e69eda0fd2c9554e3b4b48
Sho Nakatani <lay.sakura@gmail.com>
2021-08-25 10:47:55 +0900

    refactor: revert #9 since we stop using math in documents (#24)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,3 @@
-const math = require('remark-math');
-const katex = require('rehype-katex');
-
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'apllodb ',
@@ -56,23 +53,11 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/apllodb/apllodb.github.io',
-
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
-  ],
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
-      crossorigin: 'anonymous',
-    },
   ],
 };
